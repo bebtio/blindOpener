ec94d68015b0d8fbd11b4ef2cf6fafd050625926
bebtio <bebtio@gmail.com>
2022-09-09 21:13:14 -0700

    Added remote button's hexadecimal values

---
+++ arduino/irRemoteButtonDefines.hpp
@@ -0,0 +1,13 @@
+
+namespace IRremote
+{
+    const int POWER_BUTTON  = 0xB946FF00;
+    const int A_BUTTON      = 0xBB44FF00; 
+    const int B_BUTTON      = 0xBF40FF00; 
+    const int C_BUTTON      = 0xBC43FF00; 
+    const int UP_BUTTON     = 0xE619FF00;
+    const int RIGHT_BUTTON  = 0xA15EFF00;
+    const int DOWN_BUTTON   = 0xE31CFF00;
+    const int LEFT_BUTTON   = 0xF30CFF00;
+    const int CENTER_BUTTON = 0xE718FF00;
+}
