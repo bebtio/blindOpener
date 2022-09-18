e6cf629f5f35bc247c1ef613995d282291529ae0
bebtio <bebtio@gmail.com>
2022-09-18 09:15:12 -0700

    Added define file for easy pin selections

---
+++ arduino/arduinoPinDefines.hpp
@@ -0,0 +1,33 @@
+#ifndef _ARDUINO_UNO_PINDEFINES_HPP_
+#define _ARDUINO_UNO_PINDEFINES_HPP_
+
+// Easy enough, they just map to their number.
+// The defines are what they are labeled on the board.
+// Their values are what they are in code.
+
+// Digital only pins. 
+#define D2   2 
+#define D3   3 
+#define D4   4 
+#define D5   5
+#define D6   6
+#define D7   7
+#define D8   8
+#define D9   9
+#define D10 10
+#define D11 11
+#define D12 12
+#define D13 13
+
+// Can be used for both analog and digital pins.
+#define A0  14
+#define A1  15
+#define A2  16
+#define A3  17
+#define A4  18
+#define A5  19
+
+
+
+
+#endif // _ARDUINO_UNO_PINDEFINES_HPP_
