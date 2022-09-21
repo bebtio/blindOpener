4f1abb210fce631b3a0d7a80c7063c762ca4d95b
bebtio <bebtio@gmail.com>
2022-09-20 21:37:01 -0700

    Added stepper motor State machine class. The goal is to move the StepperMotor into this class along with all the logic to control it.

---
+++ arduino/StepperMotorStateMachine.cpp
@@ -0,0 +1,6 @@
+#include "StepperMotorStateMachine.hpp"
+
+StepperMotorStateMachine::StepperMotorStateMachine()
+{
+  
+}
---
+++ arduino/StepperMotorStateMachine.hpp
@@ -0,0 +1,16 @@
+#ifndef _STEPPER_MOTOR_STATE_MACHINE_HPP_
+#define _STEPPER_MOTOR_STATE_MACHINE_HPP_
+
+class StepperMotorStateMachine
+{
+public:
+  enum StepperMotorStates
+  {
+    
+  };
+  
+  StepperMOtorStateMachine();
+
+};
+
+#endif // _STEPPER_MOTOR_STATE_MACHINE_HPP_
